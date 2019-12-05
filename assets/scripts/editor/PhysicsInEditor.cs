38ee4f891ae026e6a25ed81706c95ddf112ff872
Austin Taylor <austin@teamtaylor.org>
2019-12-04 22:59:59 -0500

    Added Sea Of Metal
    current state: large on bottom, small floating above before simulated

---
+++ assets/scripts/editor/PhysicsInEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PhysicsInEditor : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ye");
+    }
+}
