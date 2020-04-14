a8478ca48fa4c2372217dbd2aa1961da4d04f265
Austin Taylor <austin@teamtaylor.org>
2020-04-13 23:02:06 -0400

    Added luthadel wealthier building

--- Assets/Scripts/Environment/Scenes/Environment_TestingGrounds.cs
+++ Assets/Scripts/Environment/Scenes/Environment_TestingGrounds.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Environment_TestingGrounds : MonoBehaviour {
 
+    private bool done;
+
     void Start() {
+        done = false;
         GameManager.ConversationManager.StartConversation("CASUAL_CHAT");
     }
     void Update() {
         if(!HUD.ConversationHUDController.IsOpen) {
-            if(Keybinds.AdvanceConversation()) {
+            if(Keybinds.AdvanceConversation() && !done) {
+                done = true;
                 GameManager.ConversationManager.StartConversation("STILL_HERE");
             }
         }
