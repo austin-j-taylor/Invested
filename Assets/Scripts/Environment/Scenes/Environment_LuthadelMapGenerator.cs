0f1e278ac55676158fb1d505ca0ce36d3ed18334
Austin Taylor <austin@teamtaylor.org>
2020-05-15 18:56:27 -0400

    Start of placing houses in luthadel algorithm

---
+++ Assets/Scripts/Environment/Scenes/Environment_LuthadelMapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Environment_LuthadelMapGenerator : Environment {
+
+    [SerializeField]
+    private GameObject house;
+    [SerializeField]
+    private Texture2D map;
+    [SerializeField]
+    private float mapWidth, mapHeight;
+
+    public void PlaceHouse() {
+
+        Color[] pixels = map.GetPixels();
+        int height = map.height;
+        int width = map.width;
+        for(int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if(i == j) {
+                    Vector2 position = new Vector2(j / width * mapWidth, i / height * mapHeight);
+                    Instantiate(house, position, Quaternion.identity, transform);
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Environment/Scenes/LuthadelMapGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Environment_LuthadelMapGenerator))]
+public class LuthadelMapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        Environment_LuthadelMapGenerator myScript = (Environment_LuthadelMapGenerator)target;
+        if (GUILayout.Button("Build House")) {
+            myScript.PlaceHouse();
+        }
+    }
+
+
+}
