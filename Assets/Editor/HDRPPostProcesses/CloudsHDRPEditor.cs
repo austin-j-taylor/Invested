f053966e28e292260e204c132b1d8bb230143a11
Austin Taylor <austin@teamtaylor.org>
2020-08-06 16:52:50 -0400

    Skeleton for upgrading clouds to HDRP

---
+++ Assets/Editor/HDRPPostProcesses/CloudsHDRPEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEditor;
+
+[VolumeComponentEditor(typeof(CloudsHDRP))]
+
+sealed class GrayScaleEditor : VolumeComponentEditor {
+
+    SerializedDataParameter m_Intensity;
+
+    public override bool hasAdvancedMode => false;
+
+    public override void OnEnable() {
+        base.OnEnable();
+        PropertyFetcher<CloudsHDRP> o = new PropertyFetcher<CloudsHDRP>(serializedObject);
+        m_Intensity = Unpack(o.Find(x => x.intensity));
+    }
+
+    public override void OnInspectorGUI() {
+        GUILayout.Label("Cloud Parameters");
+        PropertyField(m_Intensity);
+    }
+}
