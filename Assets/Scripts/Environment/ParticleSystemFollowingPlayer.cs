6b8d7678243d3d834c33614d2449ba02e40942c3
Austin Taylor <austin@teamtaylor.org>
2020-12-22 21:37:30 -0500

    Starting replacing computationally intensive volumetric clouds with more efficient particle systems
    Title screen, tutorial 1

---
+++ Assets/Scripts/Environment/ParticleSystemFollowingPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemFollowingPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private bool followVertically = true;
+
+    private float startY;
+
+    private void Start() {
+        startY = transform.position.y;
+        Update();
+        ParticleSystem particles = GetComponent<ParticleSystem>();
+        particles.Stop();
+        particles.Play();
+    }
+
+    private void Update() {
+        Vector3 position = Player.PlayerInstance.transform.position;
+
+        if (!followVertically) {
+            position.y = startY;
+        }
+        transform.position = position;
+    }
+}
